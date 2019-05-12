8f67972c0ea330bb7b41e78483e74925f1f8bc26
MrHope312 <shrey@hope.com>
2019-05-12 18:27:15 +0530

    updates

--- app/index.js
+++ app/index.js
@@ -1,42 +1,46 @@
+//node module calls
 const express = require('express');
 const bodyParser = require('body-parser');
+var path = require('path');
+var logger = require('morgan');
+var cors = require('cors')
+
+//module calls
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server.js');
-
-
 const Wallet = require('../wallet');
-
-
 const TransactionPool = require('../wallet/transaction-pool');
-
 const Miner = require('./miner');
-
-const HTTP_PORT = process.env.HTTP_PORT || 3001;
-
-const app = express();
+//router calls
+var indexRouter = require('../routes/index');
+//function declarations
 const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc, tp);
 const miner = new Miner(bc, tp, wallet, p2pServer);
+//port declaratiom
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+//express app call
+const app = express();
+// view engine setup
 
-
-
-
-
-
+app.set('view engine', 'ejs');
+app.use(cors())
+app.use(logger('dev'));
 
 app.use(bodyParser.json());
+//router declarations
+app.use('/', indexRouter);
 
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
   //console.log(res);
-  // p2pServer.syncChains();
 });
 
 app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data);
-  // console.log(`New block added: ${block.toString()}`);
+  console.log(`New block added: ${block.toString()}`);
   p2pServer.syncChains();
   res.redirect('/blocks');
 });
@@ -50,7 +54,7 @@ app.post('/transact', (req, res) => {
     recipient,
     amount
   } = req.body;
-  const transaction = wallet.createTransaction(recipient, amount,bc, tp);
+  const transaction = wallet.createTransaction(recipient, amount, bc, tp);
   p2pServer.broadcastTransaction(transaction);
   res.redirect('/transactions');
 
